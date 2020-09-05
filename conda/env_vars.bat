1a693536860c6bd422cf3b7c9018cb3383a1668d
Christian López Barrón <chris.gfz@gmail.com>
2020-09-05 22:52:35 +1000

    add blender + usd activate file

---
+++ conda/env_vars.bat
@@ -0,0 +1,3 @@
+set PYTHONPATH=B:/write/builds/py37usd208build/lib/python
+set PATH=%PATH%;B:\write\builds\py37usd208build\bin;B:\write\builds\py37usd208build\lib
+DOSKEY blend="C:\Program Files\Blender Foundation\Blender 2.90\blender.exe" --python-use-system-env
