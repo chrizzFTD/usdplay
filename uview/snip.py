7a1c8161bcab25a4e8cb6065103920fc8ad7895d
Christian López Barrón <chris.gfz@gmail.com>
2020-09-07 00:34:42 +1000

    reportModelInfo and test various available usd scenes

--- uview/snip.py
+++ uview/snip.py
@@ -1,16 +1,52 @@
-import immutables
 from pxr import Usd, Ar
-from pprint import pp
 from pathlib import Path
-from operator import itemgetter
-fp = r"B:\read\cg\downloads\Kitchen_set\Kitchen_set\Kitchen_set_instanced.usd"
-stage = Usd.Stage.Open(fp)
-models = map(Usd.ModelAPI, stage.Traverse(predicate=Usd.PrimIsModel))
-infos = {info for model in models if (info := immutables.Map(model.GetAssetInfo()))}
+from functools import lru_cache
+from collections import defaultdict
 
-_resolveAr = Ar.GetResolver().Resolve
-def resolved(assetPath):
-    return _resolveAr(str(Path(str(assetPath.path))))
 
-with Ar.ResolverContextBinder(stage.GetPathResolverContext()):
-    pp(sorted((dict(i, resolvedPath=resolved(i['identifier'])) for i in infos), key=itemgetter('name')))
+def reportModelInfo(stage):
+    _resolveAr = Ar.GetResolver().Resolve
+
+    @lru_cache
+    def resolve(identifier):
+        return _resolveAr(str(Path(identifier)))
+
+    missing_info = set()
+    incomplete_info = defaultdict(set)
+    unresolved_ids = defaultdict(set)
+
+    with Ar.ResolverContextBinder(stage.GetPathResolverContext()):
+        for prim in stage.Traverse(predicate=Usd.PrimIsModel):
+            model = Usd.ModelAPI(prim)
+            if not (model.IsKind("component") or model.IsKind("assembly")):
+                continue  # only check for "important" kinds
+            elif (info := model.GetAssetInfo()):
+                if (missing := frozenset({"name", "identifier"} - set(info))):
+                    incomplete_info[missing].add(prim)
+                elif not resolve(info['identifier'].path):
+                    unresolved_ids[info['identifier']].add(prim)
+                continue
+            missing_info.add(prim)
+
+    return missing_info, incomplete_info, unresolved_ids
+
+
+if __name__ == "__main__":
+    from printree import ptree
+    for filepath in {
+        r"B:\read\cg\downloads\Kitchen_set\Kitchen_set\Kitchen_set.usd",
+        r"B:\read\cg\downloads\Kitchen_set\Kitchen_set\Kitchen_set_instanced.usd",
+        
+        r"B:\read\cg\downloads\UsdSkelExamples\UsdSkelExamples\HumanFemale\HumanFemale.usd",
+        r"B:\read\cg\downloads\UsdSkelExamples\UsdSkelExamples\HumanFemale\HumanFemale.walk.usd",
+        r"B:\read\cg\downloads\UsdSkelExamples\UsdSkelExamples\HumanFemale\HumanFemale.keepAlive.usd",
+
+        r"B:\read\cg\downloads\PointInstancedMedCity\PointInstancedMedCity.usd",
+        r"B:\read\cg\downloads\Attic_NVIDIA_506\Attic_NVIDIA\Attic_NVIDIA.usd",
+        r"B:\read\cg\downloads\esper_room_v3\esper_room_v3\EsperRoom.usda",
+    }: 
+        # continue
+        print(f"\nChecking Model Asset Info on stage from {filepath=}")
+        stage = Usd.Stage.Open(filepath)
+        missing, incomplete, unresolved = reportModelInfo(stage)
+        ptree(dict(missing=missing, incomplete=incomplete, unresolved=unresolved))
